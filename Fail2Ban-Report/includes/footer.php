9e407f82f87cd0ecad6055a8fd8f7dad6a030e5d
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-08-12 15:26:53 +0200

    Create footer.php

---
+++ Fail2Ban-Report/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="site-footer">
+  <div class="footer-content">
+    <span title="Made with sweat, tears and love by SubleXBle">Made with 🥵, 😿 and ❤️ </span>by <a href="https://github.com/SubleXBle" title="SubleXBle on Github" target="_blank" class="yellowtext">SubleXBle</a> &copy; <?php echo date('Y'); ?> All rights reserved
+  </div>
+</footer>
